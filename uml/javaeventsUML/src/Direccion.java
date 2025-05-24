7ed6d831caa772fae7d54c4ac98c1683aa27ee54
Blockky <blockygh6@hotmail.com>
2025-05-24 16:22:08 +0200

    Proyecto creado

---
+++ uml/javaeventsUML/src/Direccion.java
@@ -0,0 +1,65 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("19f309d3-f761-4a02-9ff9-e399ae52e9b4")
+public class Direccion {
+    @objid ("662498ea-e826-4ab4-9454-4d8b33e0ab99")
+    private String calle;
+
+    @objid ("d851d4f6-6a8c-40e6-8c87-002c074b9618")
+    private int numero;
+
+    @objid ("aa9b62a2-1164-41f4-9261-73ab06f65b80")
+    private String ciudad;
+
+    @objid ("c13493fc-6f28-44bf-a489-e7e8e5ddef36")
+    private int codigoPostal;
+
+    @objid ("78d7c7dc-00e5-4d79-97f2-5b95144f3a00")
+    public String getCalle() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.calle;
+    }
+
+    @objid ("a5c9a398-30b6-45d3-8283-7fc1c98b9dcd")
+    public void setCalle(final String value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.calle = value;
+    }
+
+    @objid ("b51b5d92-975e-4017-95ca-9e1c137e41a0")
+    public int getNumero() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.numero;
+    }
+
+    @objid ("7fd64169-07ed-4d42-b22e-c3a8d68e56c4")
+    public void setNumero(final int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.numero = value;
+    }
+
+    @objid ("fa66a80d-7b6b-421a-99ba-354f8b4c6f6a")
+    public String getCiudad() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.ciudad;
+    }
+
+    @objid ("7a922174-ae08-4d1b-99a3-d68e8fcb0789")
+    public void setCiudad(final String value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.ciudad = value;
+    }
+
+    @objid ("811285a0-e679-4538-ae35-e31430368816")
+    public int getCodigoPostal() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.codigoPostal;
+    }
+
+    @objid ("2bff695f-f6c4-4b41-a9b6-e0e05bb8684e")
+    public void setCodigoPostal(final int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.codigoPostal = value;
+    }
+
+}
